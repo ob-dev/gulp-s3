dca5147b1acd7e2434845d64406ad63dd6190068
Jan Matousek <jan@m6k.cz>
2014-07-25 18:46:04 +0200

    ignore directories, silent mode which shows only number of files uploaded

--- index.js
+++ index.js
@@ -7,57 +7,78 @@ var mime = require('mime');
 mime.default_type = 'text/plain';
 
 module.exports = function (aws, options) {
-  options = options || {};
+	options = options || {};
 
-  if (!options.delay) { options.delay = 0; }
+	if (!options.delay) { options.delay = 0; }
 
-  var client = knox.createClient(aws);
-  var waitTime = 0;
-  var regexGzip = /\.([a-z]{2,})\.gz$/i;
-  var regexGeneral = /\.([a-z]{2,})$/i;
+	var client = knox.createClient(aws);
+	var waitTime = 0;
+	var regexGzip = /\.([a-z]{2,})\.gz$/i;
+	var regexGeneral = /\.([a-z]{2,})$/i;
+	var filesUploaded = 0;
 
-  return es.map(function (file, callback) {
+	var stream = es.map(function (file, callback) {
+		if (file.stat.isDirectory()) {
+			return callback();
+		}
+		if (!file.isBuffer()) {
+			return callback(file); // fail if we don't know how to upload
+		}
 
-      // Verify this is a file
-      if (!file.isBuffer()) { return file; }
+		var uploadPath = file.path.replace(file.base, options.uploadPath || '');
+		uploadPath = uploadPath.replace(new RegExp('\\\\', 'g'), '/');
+		var headers = { 'x-amz-acl': 'public-read' };
+		if (options.headers) {
+			for (var key in options.headers) {
+				headers[key] = options.headers[key];
+			}
+		}
 
-      var uploadPath = file.path.replace(file.base, options.uploadPath || '');
-      uploadPath = uploadPath.replace(new RegExp('\\\\', 'g'), '/');
-      var headers = { 'x-amz-acl': 'public-read' };
-      if (options.headers) {
-          for (var key in options.headers) {
-              headers[key] = options.headers[key];
-          }
-      }
+		if (regexGzip.test(file.path)) {
+			// Set proper encoding for gzipped files, remove .gz suffix
+			headers['Content-Encoding'] = 'gzip';
+			uploadPath = uploadPath.substring(0, uploadPath.length - 3);
+		} else if (options.gzippedOnly) {
+			// Ignore non-gzipped files
+			return file;
+		}
 
-      if (regexGzip.test(file.path)) {
-          // Set proper encoding for gzipped files, remove .gz suffix
-          headers['Content-Encoding'] = 'gzip';
-          uploadPath = uploadPath.substring(0, uploadPath.length - 3);
-      } else if (options.gzippedOnly) {
-          // Ignore non-gzipped files
-          return file;
-      }
+		// Set content type based of file extension
+		if (!headers['Content-Type'] && regexGeneral.test(uploadPath)) {
+			headers['Content-Type'] = mime.lookup(uploadPath);
+			if (options.encoding) {
+				headers['Content-Type'] += '; charset=' + options.encoding;
+			}
+		}
 
-      // Set content type based of file extension
-      if (!headers['Content-Type'] && regexGeneral.test(uploadPath)) {
-        headers['Content-Type'] = mime.lookup(uploadPath);
-        if (options.encoding) {
-          headers['Content-Type'] += '; charset=' + options.encoding;
-        }
-      }
+		headers['Content-Length'] = file.stat.size;
+		client.putBuffer(file.contents, uploadPath, headers, function (err, res) {
+			if (err) {
+				gutil.log(gutil.colors.red('[FAILED]', file.path, err, res));
+				return callback(err);
+			} else if (res.statusCode !== 200) {
+				gutil.log(gutil.colors.red('[FAILED]', file.path, res.statusCode));
+				res.resume();
+				return callback("Upload error: bad status code " + res.statusCode);
+			} else {
+				if (!options.silent) {
+					gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
+				}
+				filesUploaded++;
+				res.resume();
+				callback(null, file);
+			}
+		});
+	});
 
-      headers['Content-Length'] = file.stat.size;
+	if (options.silent) {
+		stream.on('end', function () {
+			gutil.log(gutil.colors.green(
+				'[SUCCESS] uplodaded', filesUploaded,
+				'files to', options.uploadPath
+			));
+		});
+	}
 
-      client.putBuffer(file.contents, uploadPath, headers, function(err, res) {
-        if (err || res.statusCode !== 200) {
-          gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
-          callback(err, res);
-        } else {
-          gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
-          res.resume();
-          callback(null, file);
-        }
-      });
-  });
-};
+	return stream;
+}
